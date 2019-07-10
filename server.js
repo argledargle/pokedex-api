26251908fa18413138de1bbc66c7960c9f98eeb5
Alex Peter <apyells@gmail.com>
2019-07-10 10:01:17 -0500

    update security unique ID, add handleGetPokemon

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+
+console.log(process.env.API_TOKEN)
 
 const app = express()
 
@@ -12,6 +15,12 @@ function handleGetTypes(req,res) {
 }
 
 app.get('/types', handleGetTypes)
+
+function handleGetPokemon(req, res) {
+    res.send('Hello, Pokemon!')
+}
+
+app.get('/pokemon', handleGetPokemon)
 
 const PORT=8000
 
