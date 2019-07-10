dc983cf88a46a6d3048fc8bb2d901c42d6fe36ae
Alex Peter <apyells@gmail.com>
2019-07-10 16:19:20 -0500

    everything works :)

--- server.js
+++ server.js
@@ -1,42 +1,74 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const POKEDEX = require("./pokedex.json");
 
-console.log(process.env.API_TOKEN)
+const app = express();
 
-const app = express()
-
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.headers.authorization
-    
-    console.log('validate bearer token middleware')
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.headers.authorization;
 
-    if(!authToken || authToken.split(' ')[1] !== apiToken) {
-        return res.status(401).json({error:'Unauthorized request'})
-    }
+  console.log("validate bearer token middleware");
 
-    next()
-})
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
 
-const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+  next();
+});
 
-function handleGetTypes(req,res,next) {
-    res.json(validTypes)
+const validTypes = [
+  `Bug`,
+  `Dark`,
+  `Dragon`,
+  `Electric`,
+  `Fairy`,
+  `Fighting`,
+  `Fire`,
+  `Flying`,
+  `Ghost`,
+  `Grass`,
+  `Ground`,
+  `Ice`,
+  `Normal`,
+  `Poison`,
+  `Psychic`,
+  `Rock`,
+  `Steel`,
+  `Water`
+];
+
+function handleGetTypes(req, res, next) {
+  res.json(validTypes);
 }
 
-app.get('/types', handleGetTypes)
+app.get("/types", handleGetTypes);
 
 function handleGetPokemon(req, res) {
-    res.send('Hello, Pokemon!')
+  let response = POKEDEX.pokemon;
+  //filter out pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    );
+  }
+
+  //filter out pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    );
+  }
+  res.json(response)
 }
 
-app.get('/pokemon', handleGetPokemon)
+app.get("/pokemon", handleGetPokemon);
 
-const PORT=8000
+const PORT = 8000;
 
-app.listen(PORT, ()=> {
-    console.log(`Server listening at http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
