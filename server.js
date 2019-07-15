9334abea0ac401f6f5ed27be29e0e560c5015582
Alex Peter <apyells@gmail.com>
2019-07-15 09:55:31 -0500

    updates security for deployment

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const POKEDEX = require("./pokedex.json");
-const cors = require('cors')
-const helmet = require('helmet')
+const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 
-app.use(morgan("dev"));
-app.use(helmet())
-app.use(cors())
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.headers.authorization;
-
-  console.log("validate bearer token middleware");
 
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
@@ -66,13 +65,23 @@ function handleGetPokemon(req, res) {
       pokemon.type.includes(req.query.type)
     );
   }
-  res.json(response)
+  res.json(response);
 }
 
 app.get("/pokemon", handleGetPokemon);
 
+app.use((error, req, res, next) => {
+  let response
+  if(process.env.NOVE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`Server is up`);
 });
